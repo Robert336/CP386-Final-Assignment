aec3273124da9ae8855018b0a2cf2deb8dc935c4
Jagveer-Sangha <jagveersangha@gmail.com>
2021-07-24 12:20:26 -0400

    Added function that reads the sequence of numbers from within the file

--- a.c
+++ a.c
@@ -16,13 +16,19 @@ typedef struct thread //represents a single thread, you can add more members if 
     int retVal;
 } Thread;
 
-int readFile(char *fileName); //function to read the file content and build array of threads
+int readFileCustomers(char *fileName);
+void *readFileSequences(char *fileName);
 
 int main()
 {
 
-    int TotalCustomers = readFile("sample4_in.txt");
-    printf("Number of Customers: %d", TotalCustomers);
+    int TotalCustomers = readFileCustomers("sample4_in.txt");
+    printf("Number of Customers: %d\n", TotalCustomers);
+    printf("Maximum resources from file:\n");
+    void *CustomerSequences = readFileSequences("sample4_in.txt");
+
+    // Finish bankersalgo
+    // printf("Enter Command:");
 
     return 0;
 }
@@ -30,7 +36,7 @@ int main()
 /*
 *Program should automatically read the text file and count the 'customers' (sequence of numbers)
 */
-int readFile(char *fileName)
+int readFileCustomers(char *fileName)
 {
     FILE *in = fopen("sample4_in.txt", "r");
 
@@ -45,8 +51,10 @@ int readFile(char *fileName)
     fstat(fileno(in), &st);
     char *fileContent = (char *)malloc(((int)st.st_size + 1) * sizeof(char));
     fileContent[0] = '\0';
+
     while (!feof(in))
     {
+
         count += 1;
         char line[100];
         if (fgets(line, 100, in) != NULL)
@@ -56,6 +64,28 @@ int readFile(char *fileName)
     }
     fclose(in);
     return count;
+}
+
+void *readFileSequences(char *fileName)
+{
+    FILE *in = fopen("sample4_in.txt", "r");
+
+    if (!in)
+    {
+        printf(
+            "Error in opening input file...exiting with error code -1\n");
+        return NULL;
+    }
+    while (!feof(in))
+    {
+        char line[100];
+        if (fgets(line, 100, in) != NULL)
+        {
+            printf("%s", line);
+        }
+    }
+    fclose(in);
+    return NULL;
 }
 
 int bankersalgo()
